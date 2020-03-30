ac837dce6a923e96212490c131e4b8295dc2e921
Tobias Lerche Fodstad <tobiaf17@student.uia.no>
2020-03-30 15:04:13 +0200

    oppdaterig long og ekstern funksjon for generering av e og d

--- RSA_testing/main.cpp
+++ RSA_testing/main.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
 #include<math.h>
+#include <cstdio>
+#include <iostream>
+
 using namespace std;
 // find gcd
 int gcd(int a, int b) {
@@ -13,51 +15,58 @@ int gcd(int a, int b) {
         b= t;
     }
 }
-int main() {
-    //2 random prime numbers, the ones written are for testing
-    // double p = 69697;
-    //double q = 420691;
-    double p = 107;
-    double q = 113;
 
-    double n=p*q;//calculate n
-    double track = 0;
-    double track1 = 0;
-
-    double phi= (p-1)*(q-1);//calculate phi
-
-    double d=0;
-    double d1=0;
-    //public key
-    //e stands for encrypt
-    double e=3;
+// Kjører en egen funksjon for valg av nøkler
+long decision (long e, long d, long d1, long phi, long track, long track1, long n){
     //for checking that 1 < e < phi(n) and gcd(e, phi(n)) = 1; i.e., e and phi(n) are coprime.
-    while(1<e<phi) {
-        track = gcd(e,phi);
-        if(track==1){
-            track1 = gcd(e,n);
+    while(1<e<phi) {                 //Nøkkel må være mellom 1 og phi
+        track = gcd(e,phi);           //Kjører gcd funksjon
+        if(track==1){                 // track==1 betyr at det er coprime
+            track1 = gcd(e,n);    //Sjekker om e er coprime med n (med premiss om at den er coprime med phi)
             track = gcd(e,phi);
-            if (track1==1 && track==1){
-                d=e+1;
+            if (track1==1 && track==1){       //Må værecoprime med både phi og n
+                d=e+1;                        //dekryptering skal være større enn e
                 for (int i = 0; i < phi; ++i) {
-                    d1 = fmod(e*d, phi);
-                    if (d1==1){
+                    d1 = fmod(e*d, phi);          //Formel som må være oppfylt for at d skal fungere
+                    if (d1==1 && d!=0){
                         cout<<"\n"<<"e is:"<<e;
                         cout<<"\n"<<"d is:"<<d;
-                        break;
+                        return d,e;
                     }else{
                         d++;
                     }
                 }
-                break;
             } else
                 e++;
-            //break;
-            break;
         }
         else
             e++;
     }
+}
+
+int main() {
+    //2 random prime numbers
+    // double p = 69697;
+    //double q = 420691;
+    long p = 7;
+    long q = 11;
+
+    long n=p*q;//calculate n
+    long track = 0;
+    long track1 = 0;
+
+    long phi= (p-1)*(q-1);//calculate phi
+
+    long d=0;
+    long d1=0;
+    //e stands for encrypt
+    long e=3;
+    int breaak = 1;
+    long temp_e = 13;       //Hardkodet versjon av d og e. Funksjonen som velger nøkler vil ikke returnere dem som de skal.
+    long temp_d = 37;
+
+    decision(e,d,d1,phi, track, track1, n);
+
     //private key
     //d stands for decrypt
     //choosing d such that it satisfies d*e = 1 mod phi
@@ -69,21 +78,21 @@ int main() {
     //cout<<"\n"<<"Decryption1 key = "<<d1;
     //cout<<"\n"<<"Decryption key = "<<d;
 
-    double message;
+    //Manuell input for testing (Her skal måledataen inn når koden skal over på arduino
+    long message;
     printf("\n Write your message here\n");
-    scanf("%lf",&message);
+    scanf("%li",&message);
 
-    //double enc;                 //Ensure that the encryption is not manipulated
-    //double c = pow(message,e);  //encrypt the message
-    //c = fmod(c,n);
-    //enc=fmod(c,n);            //Encryption part two
 
-    double c = pow(message, e);
-    cout<<"\n"<<"1 Encrypted message = "<<c;
+    //Denne delen fungerer ikke som den skal uavhengig av om jeg bruker float eller long
+    //Creating the cipher text (m^e)%n
+    long c = pow(message, temp_e);
     c= fmod(c, n);
-    double m = pow(c,d);        //Decrypting message
-    cout<<"\n"<<"1 Decrypted message = "<<m;
+
+    //Decrypting the cipher text (c^d)%n. Dette fungerer ikke som det skal.
+    long m = pow(c,temp_d);        //Decrypting message
     m = fmod(m, n);
+
     //m=fmod(m,n);                //Decryption part two
 
 
@@ -91,7 +100,7 @@ int main() {
     cout<<"\n Original Message = "<<message;
     cout<<"\n"<<"Encrypted message = "<<c;
     cout<<"\n"<<"Decrypted message = "<<m;
-    cout<<"\n"<<"e is:"<<e;
+    cout<<"\n"<<"e is:"<<e;                     //E og d oppdateres ikke av funksjonen "decision"
     cout<<"\n"<<"d is:"<<d;
     cout<<"\n"<<"Track = "<<track;
     cout<<"\n"<<"n = "<<n;
